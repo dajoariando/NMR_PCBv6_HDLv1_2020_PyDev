added reference (disconnected matching network) for S11 wobble computation to correct variation due to the transmit signal that has frequency-dependent amplitude. The S11 is then simply signal/reference.

MAIN_nmr_code/nmr_std_function/data_parser.py
@@ -93,6 +93,8 @@ def read_hex_float( file_path ):
 
     f = open( file_path , 'rb' )  # read as binary
     rddata = f.read()  # read data in bytes
+    f.close()
+
     rddata = bytearray( rddata )  # convert to bytearray
     rddata = np.array( rddata )  # convert to numpy integer array
     rddata = np.array( rddata ).reshape( len( rddata ) >> 2, 4 )  # restructure to have 4 bytes per float number
@@ -100,7 +102,26 @@ def read_hex_float( file_path ):
     # convert data to float
     data = np.zeros( len( rddata ), dtype = 'float' )
     for i in range( len( rddata ) ):
-        data[i] = struct.unpack( 'f', rddata[i] )[0]
+        data[i] = struct.unpack( 'f', rddata[i] )[0]  # 'f' is float datatype
+
+    return data
+
+
+def read_hex_int16 ( file_path ):
+    import struct
+
+    f = open( file_path , 'rb' )  # read as binary
+    rddata = f.read()  # read data in bytes
+    f.close()
+
+    rddata = bytearray( rddata )  # convert to bytearray
+    rddata = np.array( rddata )  # convert to numpy integer array
+    rddata = np.array( rddata ).reshape( len( rddata ) >> 1, 2 )  # restructure to have 2 bytes per int16 number
+
+    # convert data to float
+    data = np.zeros( len( rddata ), dtype = 'int16' )
+    for i in range( len( rddata ) ):
+        data[i] = struct.unpack( 'H', rddata[i] )[0]  # 'H' is unsigned short integer datatype
 
     return data
 

MAIN_nmr_code/nmr_std_function/nmr_functions.py
@@ -13,7 +13,14 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
-def compute_wobble( nmrObj, data_parent_folder, meas_folder, s11_min, en_fig, fig_num ):
+def compute_wobble( nmrObj, data_parent_folder, meas_folder, s11_min, S11mV_ref, useRef, en_fig, fig_num ):
+
+    # S11mV_ref is the reference s11 corresponds to maximum reflection (can be made by totally untuning matching network, e.g. disconnecting all caps in matching network)
+    # useRef uses the S11mV_ref as reference, otherwise it will use the max signal available in the data
+
+    # settings
+    binary_OR_ascii = 1  # put 1 if the data file uses binary representation, otherwise it is in ascii format
+
     data_folder = ( data_parent_folder + '/' + meas_folder + '/' )
 
     ( param_list, value_list ) = data_parser.parse_info( 
@@ -27,13 +34,17 @@ def compute_wobble( nmrObj, data_parent_folder, meas_folder, s11_min, en_fig, fi
 
     file_name_prefix = 'tx_acq_'
     freqSw = np.arange( freqSta, freqSto + ( freqSpa / 2 ), freqSpa )  # plus freqSpa/2 is to include the endpoint (just like what the C does)
-    S11 = np.zeros( len( freqSw ) )
+    S11mV = np.zeros( len( freqSw ) )
     S11_ph = np.zeros( len( freqSw ) )
     for m in range( 0, len( freqSw ) ):
         # for m in freqSw:
         file_path = ( data_folder + file_name_prefix +
                      '{:4.3f}'.format( freqSw[m] ) )
-        one_scan = np.array( data_parser.read_data( file_path ) )
+
+        if binary_OR_ascii:
+            one_scan = data_parser.read_hex_int16( file_path )  # use binary representation
+        else:
+            one_scan = np.array( data_parser.read_data( file_path ) )  # use ascii representation
 
         os.remove( file_path )  # delete the file after use
 
@@ -48,11 +59,15 @@ def compute_wobble( nmrObj, data_parent_folder, meas_folder, s11_min, en_fig, fi
         # BETTER METHOD: find reflection signal peak around the bandwidth
         ref_idx = ( abs( spectx - freqSw[m] ) <= ( spect_bw / 2 ) )
 
-        # S11[m] = max( abs( specty[ref_idx] ) )  # find reflection peak
-        S11[m] = np.mean( abs( specty[ref_idx] ) )  # compute the mean of amplitude inside RBW
+        # S11mV[m] = max( abs( specty[ref_idx] ) )  # find reflection peak
+        S11mV[m] = np.mean( abs( specty[ref_idx] ) )  # compute the mean of amplitude inside RBW
         S11_ph[m] = np.mean( np.angle( specty[ref_idx] ) ) * ( 360 / ( 2 * np.pi ) )
 
-    S11dB = 20 * np.log10( S11 / max( S11 ) )  # convert to dB scale
+    if useRef:  # if reference is present
+        S11dB = 20 * np.log10( np.divide( S11mV, S11mV_ref ) )  # convert to dB scale
+    else:  # if reference is not present
+        S11dB = 20 * np.log10( S11mV / max( S11mV ) )  # convert to dB scale
+
     S11_min10dB = ( S11dB <= s11_min )
 
     minS11 = min( S11dB )
@@ -74,7 +89,7 @@ def compute_wobble( nmrObj, data_parent_folder, meas_folder, s11_min, en_fig, fi
         fig.clf()
         ax = fig.add_subplot( 211 )
         line1, = ax.plot( freqSw, S11dB, 'r-' )
-        ax.set_ylim( -35, 0 )
+        ax.set_ylim( -35, 10 )
         ax.set_ylabel( 'S11 [dB]' )
         ax.set_title( "Reflection Measurement (S11) Parameter" )
         ax.grid()
@@ -91,13 +106,16 @@ def compute_wobble( nmrObj, data_parent_folder, meas_folder, s11_min, en_fig, fi
 
         plt.savefig( data_folder + 'wobble.png' )
 
-    # write S11 to a file
-    with open( data_folder + 'S11.txt', 'w' ) as f:
+    # write S11mV to a file
+    with open( data_folder + 'S11mV.txt', 'w' ) as f:
         for ( a, b, c ) in zip ( freqSw, S11dB, S11_ph ):
             f.write( '{:-8.3f},{:-8.3f},{:-7.1f}\n' .format( a, b, c ) )
 
     # print(S11_fmin, S11_fmax, S11_bw)
-    return S11, S11_fmin, S11_fmax, S11_bw, minS11, minS11_freq
+    if useRef:
+        return S11dB, S11_fmin, S11_fmax, S11_bw, minS11, minS11_freq
+    else:
+        return S11mV, S11_fmin, S11_fmax, S11_bw, minS11, minS11_freq
 
 
 def compute_gain( nmrObj, data_parent_folder, meas_folder, en_fig, fig_num ):

MAIN_nmr_code/nmr_wobble_searchTable.py
@@ -49,10 +49,10 @@ from faulthandler import disable
 data_parent_folder = "/root/NMR_DATA"
 en_remote_dbg = 0
 fig_num = 1
-en_fig = False  # enable figure for every measurement
-keepRawData = True  # set this to keep the S11 raw data in text file
+en_fig = 1  # enable figure for every measurement
+keepRawData = 1  # set this to keep the S11 raw data in text file
 tblMtchNtwrk = '/hw_opt/PARAM_NMR_AFE_v6.csv'  # table for the capacitance matching network capacitance values
-meas_time = 1  # measure time
+meas_time = 0  # measure time
 
 # acquisition settings (frequency to be shown in the table
 freqSta = 2
@@ -62,12 +62,12 @@ freqSamp = 25
 freqSw = np.arange( freqSta, freqSto + ( freqSpa / 2 ), freqSpa )  # plus half is to remove error from floating point number operation
 
 # frequency of interest for S11 to be optimized (range should be within frequencies in the acquisition settings
-S11FreqSta = 3.5
-S11FreqSto = 6
+S11FreqSta = 4.5
+S11FreqSto = 4.7
 
 # sweep precision
 cparPrec = 10  # change cpar by this value.
-cserPrec = 5  # change cser by this value.
+cserPrec = 10  # change cser by this value.
 
 # initial point options. either provide the L and R values, or provide with initial cpar and cser values
 lrSeed = 0  # put this to 1 if inductance of the coil is available
@@ -89,6 +89,9 @@ S11_min = -10  # the minimum allowable S11 value to be reported as adequate to s
 #    printf( 'search mode is findMinMin. Search stops when S11 of {:0.2f}dB is found.'.format( S11_min ) )
 # elif searchMode == findAbsMin:  # find the absolute minimum given the frequency range
 #    printf( 'search mode is findAbsMin.' )
+
+# global variable
+exptnum = 0  # this number is automatically increased when runExpt() is called
 
 # instantiate nmr object
 nmrObj = tunable_nmr_system_2018( data_parent_folder, en_remote_dbg )
@@ -140,7 +143,15 @@ def updateTable( Table, newVal, setting1, setting2 ):
     return Table
 
 
-def runExpt( cparVal, cserVal, minReflxTable ):
+def runExpt( cparVal, cserVal, S11mV_ref, useRef ):
+    # useRef: use the pregenerated S11mV_ref as a reference to compute reflection. If this option is 0, then the compute_wobble will instead generated S11 in mV format instead of dB format
+
+    # global variable
+    global exptnum
+    global minReflxTable
+
+    exptnum = exptnum + 1
+
     if meas_time:
         start_time = time.time()
 
@@ -166,7 +177,7 @@ def runExpt( cparVal, cserVal, minReflxTable ):
 
     # compute the generated data
     meas_folder = parse_simple_info( data_parent_folder, 'current_folder.txt' )
-    S11mV, S11_fmin, S11_fmax, S11_bw, minS11, minS11_freq = compute_wobble( nmrObj, data_parent_folder, meas_folder[0], S11_min, en_fig, fig_num )
+    S11dB, S11_fmin, S11_fmax, S11_bw, minS11, minS11_freq = compute_wobble( nmrObj, data_parent_folder, meas_folder[0], S11_min, S11mV_ref, useRef, en_fig, fig_num )
     print( '\t\tfmin={:0.3f} fmax={:0.3f} bw={:0.3f} minS11={:0.2f} minS11_freq={:0.3f} cparVal={:d} cserVal={:d}'.format( S11_fmin, S11_fmax, S11_bw, minS11, minS11_freq, cparVal, cserVal ) )
 
     if meas_time :
@@ -174,12 +185,12 @@ def runExpt( cparVal, cserVal, minReflxTable ):
         print( "### time elapsed for compute_wobble: %.3f" % ( elapsed_time ) )
 
     # update the table
-    minReflxTable = updateTable( minReflxTable, S11mV, cparVal, cserVal )
+    minReflxTable = updateTable( minReflxTable, S11dB, cparVal, cserVal )
 
     # move the measurement folder to the main folder
     swfolder_ind = swfolder + '/' + str( 'Cp_[{:d}]__Cs_[{:d}]'.format( cparVal, cserVal ) )
     if en_fig:
-        shutil.move( data_parent_folder + '/' + meas_folder[0] + '/wobble.png', swfolder + '/' + str( 'plot_Cp_[{:d}]__Cs_[{:d}].png'.format( cparVal, cserVal ) ) )  # move the figure
+        shutil.move( data_parent_folder + '/' + meas_folder[0] + '/wobble.png', swfolder + '/' + str( 'plt{:03d}_Cp_[{:d}]__Cs_[{:d}].png'.format( exptnum, cparVal, cserVal ) ) )  # move the figure
 
     if keepRawData:
         # write gain values to a file
@@ -187,21 +198,23 @@ def runExpt( cparVal, cserVal, minReflxTable ):
         RawDataFile.write( 'freq(MHz)\t min-voltage(mV)\n' )
         RawDataFile.close()
         with open( swfolder + '/S11_Cp_[{:d}]__Cs_[{:d}].txt'.format( cparVal, cserVal ), 'a' ) as f:
-            for ( a, b ) in zip ( freqSw, S11mV ):
+            for ( a, b ) in zip ( freqSw, S11dB ):
                 f.write( '{:-8.3f},{:-8.3f}\n' .format( a, b ) )
 
         shutil.rmtree ( data_parent_folder + '/' + meas_folder[0] )  # remove the data folder
     else:
-        shutil.rmtree( data_parent_folder + '/' + meas_folder[0]) # remove the data folder
+        shutil.rmtree( data_parent_folder + '/' + meas_folder[0] )  # remove the data folder
 
-    return S11mV, minS11_freq, minReflxTable
+    return S11dB, minS11_freq
 
 
-def findMinS11_atCparVal( cpar, cser_iFirst, minReflxTable , cser_Prec ):
+def findMinS11_atCparVal( cpar, cser_iFirst , cser_Prec, s11mV_ref ):
+    global minReflxTable
+
     print( "\tStart findMinS11_atCparVal()" )
-    S11mV, minS11Freq, minReflxTable = runExpt( cpar, cser_iFirst , minReflxTable )
+    S11dB, minS11Freq = runExpt( cpar, cser_iFirst , s11mV_ref , 'True' )
 
-    MinS11mV = np.min( S11mV )  # find the minimum S11 value
+    MinS11mV = np.min( S11dB )  # find the minimum S11 value
     cser_ret = cser_iFirst  # the cser return value, update when less S11 value is found
 
     print( "\tDecrement cser." )
@@ -211,16 +224,16 @@ def findMinS11_atCparVal( cpar, cser_iFirst, minReflxTable , cser_Prec ):
         cser_i = decr_cFarad_by_at_least( cser_Prec, cser_i, CsTbl )  # decrement cser_i by at least cser_Prec
         if cser_i <= 0:
             break
-        S11mVCurr, minS11FreqCurr, minReflxTable = runExpt( cpar, cser_i, minReflxTable )
-        MinS11mVCurr = np.min( S11mVCurr )
-        if MinS11mVCurr < MinS11mV: # find if current S11 is better than the previous one
+        S11dBCurr, minS11FreqCurr = runExpt( cpar, cser_i , s11mV_ref, 'True' )
+        MinS11mVCurr = np.min( S11dBCurr )
+        if MinS11mVCurr < MinS11mV:  # find if current S11 is better than the previous one
             minS11Freq = minS11FreqCurr
             MinS11mV = MinS11mVCurr
             cser_ret = cser_i
             searchIncr = False
         else:
             if ( not searchIncr ):
-                return minS11Freq, cser_ret, minReflxTable
+                return minS11Freq, cser_ret
             else:
                 break
 
@@ -230,19 +243,24 @@ def findMinS11_atCparVal( cpar, cser_iFirst, minReflxTable , cser_Prec ):
         cser_i = incr_cFarad_by_at_least( cser_Prec, cser_i, CsTbl )  # decrement cser_i by at least cser_Prec
         if cser_i > 2 ** len( CsTbl ) - 1:  # stop if the cser is more than max index of the table
             break
-        S11mVCurr, minS11FreqCurr, minReflxTable = runExpt( cpar, cser_i , minReflxTable )
-        MinS11mVCurr = np.min( S11mVCurr )
-        if MinS11mVCurr < MinS11mV: # find if current S11 is better than the previous one
+        S11dBCurr, minS11FreqCurr = runExpt( cpar, cser_i, s11mV_ref, 'True' )
+        MinS11mVCurr = np.min( S11dBCurr )
+        if MinS11mVCurr < MinS11mV:  # find if current S11 is better than the previous one
             minS11Freq = minS11FreqCurr
             MinS11mV = MinS11mVCurr
             cser_ret = cser_i
         else:
             break
 
-	return minS11Freq, cser_ret, minReflxTable
+    return minS11Freq, cser_ret
+
+
+# find reference
+print( 'Generate reference.' )
+S11mV_ref, minS11Freq_ref = runExpt( 0, 0, 0, 0 )  # background is computed with no capacitor connected -> max reflection
 
 # find initial seed, and update cser_init if better cser value is found for the corresponding cpar_init
-S11minFreq, cser_init, minReflxTable = findMinS11_atCparVal( cpar_init, cser_init, minReflxTable , cserPrec )
+S11minFreq, cser_init = findMinS11_atCparVal( cpar_init, cser_init , cserPrec, S11mV_ref )
 
 # search higher frequency from seed
 cpar = cpar_init
@@ -255,7 +273,7 @@ while( True ):
         if cpar <= 0:  # stop if cpar is 0 or below
             print( 'decrementing cpar stops due to final cpar value <= 0.' )
             break
-        S11minfreqRight, cser, minReflxTable = findMinS11_atCparVal( cpar, cser, minReflxTable , cserPrec )
+        S11minfreqRight, cser = findMinS11_atCparVal( cpar, cser , cserPrec, S11mV_ref )
     else:
         print( 'decrementing cpar stops due to minimum S11 in highest frequency of interest is found.' )
         break
@@ -271,7 +289,7 @@ while ( True ):  # search lower frequency
         if cpar > 2 ** len( CpTbl ) - 1:  # stop if the cpar is more than max index of the table
             print( 'incrementing cpar stops due to final cpar value {:d} > {:d}'.format( cpar, 2 ** len( CpTbl ) - 1 ) )
             break
-        S11minfreqLeft, cser, minReflxTable = findMinS11_atCparVal( cpar, cser , minReflxTable, cserPrec )
+        S11minfreqLeft, cser = findMinS11_atCparVal( cpar, cser, cserPrec, S11mV_ref )
     else:
         print( 'incrementing cpar stops due to minimum S11 in lowest frequency of interest is found.' )
         break
@@ -286,7 +304,7 @@ Table.write( '\tAcq. Frequency Stop = {:.3f} MHz\n'.format( freqSto ) )
 Table.write( '\tAcq. Frequency Spacing = {:.3f} MHz\n'.format( freqSpa ) )
 Table.write( '\tAcq. Frequency Sampling = {:.1f} MHz\n'.format( freqSamp ) )
 Table.write( '\tS11 Optimization Frequency Start = {:.2f} MHz\n'.format( S11FreqSta ) )
-Table.write( '\tS11 Optimiztaion Frequency Stop = {:.2f} MHz\n'.format( S11FreqSto ) )
+Table.write( '\tS11 Optimization Frequency Stop = {:.2f} MHz\n'.format( S11FreqSto ) )
 if lrSeed:
     l_init = 1e-6  # coil inductance
     r_init = 0.1  # coil resistance
@@ -302,7 +320,7 @@ else:
         Table.write( '\tCser = {:d} ({:.1f} pF)\n'.format( cser_init , conv_cInt_to_cFarad( cser_init, CsTbl ) * 1e12 ) )
 
 Table.write( '\n' );
-Table.write( 'freq(MHz)\t min-voltage(mV)\t Cpar(digit)\t Cser(digit)\n' )
+Table.write( 'freq(MHz)\t S11(dB)\t Cpar(digit)\t Cser(digit)\n' )
 Table.close()
 with open( swfolder + '/genS11Table.txt', 'a' ) as Table:
     for ( a, b , c, d ) in zip ( minReflxTable[:, 0], minReflxTable[:, 1], minReflxTable[:, 2], minReflxTable[:, 3] ):
@@ -313,6 +331,6 @@ plt.figure( 2 )
 plt.plot( minReflxTable[:, 0], minReflxTable[:, 1] )
 plt.title( 'Minimum reflection' )
 plt.xlabel( 'freq (MHz)' )
-plt.ylabel( 'voltage (mV)' )
+plt.ylabel( 'S11 (dB)' )
 plt.savefig( swfolder + '/genS11Table.png' )
 plt.show()